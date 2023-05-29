03f606cf6b8e0d8d6da78a0654dd745561e1b4e3
dung5ire <dung@5ire.org>
2023-05-29 10:59:42 +0700

    chore: fixes
    Signed-off-by: dung5ire <dung@5ire.org>

--- queryTransaction.js
+++ queryTransaction.js
@@ -8,7 +8,6 @@ const MAX_CALL_WEIGHT = new BN(500_000_000_000).isub(BN_ONE);
 const PROOFSIZE = new BN(1_000_000);
 
 try {
-    let address; // variable for storing the address of the deployed contract 
 
     // API creation for connection to the chain
     const wsProvider = new WsProvider('wss://wss-testnet.5ire.network/');
@@ -29,9 +28,10 @@ try {
     const userKeyring = keyring.addFromMnemonic(PHRASE);
     // parameters for constructor function inside the contract
 
+    // Put your contract address that you already deployed
     const contract = new ContractPromise(api, json, '5Czfe1m8zPaRuwBrByyPkesgz7cE4ZUNX3dWnwAzVAKDkEVu');
 
-    //console.log(await contract.query);
+    
     // Query value from contract
     const { result, output } = await contract.query.balanceOf(
         userKeyring.address,
